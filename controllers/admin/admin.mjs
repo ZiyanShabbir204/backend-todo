c358cce0a278fee84b7e06c3c676e5e2328da2eb
SDG Internee 1 <sdg.internee1@int.sybrid.com>
2024-08-19 19:31:20 +0500

    change password and update profile

--- controllers/admin/admin.mjs
+++ controllers/admin/admin.mjs
@@ -3,6 +3,28 @@ import bcrypt from "bcrypt";
 import generateWebTokenAndSetCookies from "../../utilis/generateToken.mjs";
 import { responseHelper } from "../../utilis/responseHelper.mjs";
 import { statusHelper } from "../../utilis/statusHelper.mjs";
+
+
+
+export const getAllAdmin = async (req,res)=>{
+
+  try {
+
+    const admins =  await Admin.find()
+    if(!admins){
+      return res.status(statusHelper(4)).json(responseHelper(false,"no admin found",null))
+    }
+    return res.status(statusHelper(1)).json(responseHelper(true," admin found",admins))
+
+    
+  } catch (error) {
+    return res
+      .status(statusHelper(2))
+      .json(responseHelper(false, "internal server error", null));
+  
+  }
+
+}
 export const signup = async (req, res) => {
   const { body } = req;
   const { fullname, email, password } = body;
@@ -81,3 +103,78 @@ export const login = async (req, res) => {
       .json(responseHelper(false, "internal server error", null));
   }
 };
+
+export const changePassword = async (req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  const { password, newPassword } = body;
+  try {
+    if (!password || !newPassword) {
+      return res
+        .status(statusHelper(3))
+        .json(responseHelper(false, "invalid Fieldsss", null));
+    }
+    const admin = await Admin.findById(id).select("password");
+    // console.log("admin",admin)
+    const isPasswordCorrect = await bcrypt.compare(
+      password,
+      admin?.password || " "
+    );
+    if (!isPasswordCorrect || !admin) {
+      return res
+        .status(statusHelper(3))
+        .json(responseHelper(false, "Incorrect Password", null));
+    }
+    const salt = await bcrypt.genSalt(10);
+
+    const hashedPassword = await bcrypt.hash(newPassword, salt);
+    const temp = {  
+      password: hashedPassword,
+    };
+    const newAdmin = await Admin.findByIdAndUpdate(id, temp, { new: true }).select("password");
+   
+    return res
+      .status(statusHelper(1))
+      .json(responseHelper(true, "password change successfully", newAdmin));
+  } catch (error) {
+    return res
+      .status(statusHelper(2))
+      .json(responseHelper(false, "internal server error", null));
+  }
+};
+
+
+export const updateProfile = async (req,res)=>{
+  const { id } = req.params;
+  const { body } = req;
+  const { fullname, email } = body;
+  try {
+    if (!fullname || !email) {
+      return res
+        .status(statusHelper(3))
+        .json(responseHelper(false, "invalid Fieldsss", null));
+    }
+    const admin = await Admin.findById(id)
+    if(!admin){
+      return res
+        .status(statusHelper(3))
+        .json(responseHelper(false, "Admin not exists", null));
+
+    }
+    const temp = {
+      fullname,
+      email
+    }
+
+    const newAdmin = await Admin.findByIdAndUpdate(id,temp,{new:true})
+    return res
+    .status(statusHelper(1))
+    .json(responseHelper(true, "user profile updated", newAdmin));
+
+  } catch (error) {
+    return res
+      .status(statusHelper(2))
+      .json(responseHelper(false, "internal server error", null));
+    
+  }
+}
--- routes/admin/admin.mjs
+++ routes/admin/admin.mjs
@@ -1,10 +1,14 @@
 import express, { Router } from "express"
-import { login, signup } from "../../controllers/admin/admin.mjs"
+import { login, signup ,changePassword,getAllAdmin, updateProfile} from "../../controllers/admin/admin.mjs"
+import protectRoute from "../../middleware/protect.routes.js"
 
 const router = Router()
 
+router.get("/",getAllAdmin)
 router.post("/signup",signup)
 router.post("/login",login)
+router.put("/changepassword/:id",protectRoute,changePassword)
+router.put("/updateprofile/:id",protectRoute,updateProfile)
 
 export default router
 
